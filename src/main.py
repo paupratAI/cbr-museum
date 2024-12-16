d383b8d08cd88efd1bfdfa2779fe6fd4f4f96ae9
caiselvass <caiselvassala@gmail.com>
2024-12-16 23:58:19 +0100

    ADD main test for cf

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from recommender import Recommender
 
 def main():
-	r = Recommender(cf_decay_factor=1, cf_alpha=1, cf_method='cosine', cf_gamma=1)
+	r = Recommender(cf_decay_factor=1, cf_alpha=1, cf_method='pearson', cf_gamma=1)
 	r.evaluate(reload_cf=False, save=True)
 
 if __name__ == '__main__':
